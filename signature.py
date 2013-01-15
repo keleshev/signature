e98d819ce0a95948017e28033cc4937aecbe87de
Vladimir Keleshev <halst@ya.ru>
2013-01-15 22:29:34 +0100

    New, better API based on __call__

--- signature.py
+++ signature.py
@@ -32,15 +32,15 @@ class Signature(object):
     def __getitem__(self, key):
         return list(iter(self))[key]
 
-    def __lshift__(self, other):
+    def __call__(self, *args, **kwargs):
         self.__dict__ = {'_params': self._params}  # reset __dict__
-        if len(other) > len(self._params):
+        if len(args) > len(self._params):
             raise SignatureError()  # TODO better error reporting
-        if len(other) < len([p for p in self._params if p.default is Nothing]):
+        if len(args) < len([p for p in self._params if p.default is Nothing]):
             raise SignatureError()  # TODO test
-        # Pad `other` to make it's length same as `self._params`
-        other = other + [Nothing] * (len(self._params) - len(other))
-        for param, arg in zip(self._params, other):
+        # Pad `args` to make it's length same as `self._params`
+        args = args + (Nothing,) * (len(self._params) - len(args))
+        for param, arg in zip(self._params, args):
             if arg is Nothing:
                 if param.default is Nothing:
                     assert not 'reachable'
@@ -48,7 +48,4 @@ class Signature(object):
                     self.__dict__[param.name] = param.default
             else:
                 self.__dict__[param.name] = arg
-
-
-
-
+        return self
--- test_signature.py
+++ test_signature.py
@@ -5,71 +5,50 @@ from signature import Signature, SignatureError
 
 def test_positional_parameters_vs_positional_arguments():
 
-    s = Signature('year', 'month', 'day')
+    signature = Signature('year', 'month', 'day')
 
-    s << [2013, 1, 15]
-    assert s.year  == s[0] == 2013
-    assert s.month == s[1] == 1
-    assert s.day   == s[2] == 15
-    assert list(s) == [2013, 1, 15]
-
-    with raises(SignatureError):
-        s << []
+    args = signature(*[2013, 1, 15])
+    assert args.year  == args[0] == 2013
+    assert args.month == args[1] == 1
+    assert args.day   == args[2] == 15
+    assert list(args) == [2013, 1, 15]
 
     with raises(SignatureError):
-        s << [2013, 1, 15, 20, 44]
+        signature(*[])
+    with raises(SignatureError):
+        signature(*[2013, 1, 15, 20, 44])
 
 
 def test_keyword_parameters_vs_positional_arguments():
 
-    s = Signature(('year', 1970), ('month', 1), ('day', 1))
+    signature = Signature(('year', 1970), ('month', 1), ('day', 1))
 
-    s << [2013, 1, 15]
-    assert list(s) == [2013, 1, 15]
-
-    s << [2013]
-    assert list(s) == [2013, 1, 1]
-
-    s << []
-    assert list(s) == [1970, 1, 1]
+    assert list(signature(2013, 1, 15)) == [2013, 1, 15]
+    assert list(signature(2013)) == [2013, 1, 1]
+    assert list(signature()) == [1970, 1, 1]
 
     with raises(SignatureError):
-        s << [2013, 1, 15, 20, 44]
+        signature(2013, 1, 15, 20, 44)
 
 
 def test_mixed_parameters_vs_positional_arguments():
 
-    s = Signature('year', ('month', 1), ('day', 1))
+    signature = Signature('year', ('month', 1), ('day', 1))
 
-    s << [2013, 1, 15]
-    assert list(s) == [2013, 1, 15]
-
-    s << [2013]
-    assert list(s) == [2013, 1, 1]
+    assert list(signature(2013, 1, 15)) == [2013, 1, 15]
+    assert list(signature(2013)) == [2013, 1, 1]
 
     with raises(SignatureError):
-        s << []
-
+        signature()
     with raises(SignatureError):
-        s << [2013, 1, 15, 20, 44]
-
-
-
+        signature(2013, 1, 15, 20, 44)
 
 
 def test_mixed_parameters_vs_mixed_arguments():
 
-    s = Signature('year', 'month', ('day', 1), ('hour', 0))
+    signature = Signature('year', 'month', ('day', 1), ('hour', 0))
 
     if 0:
-        s << Call(*args, **kwargs)
-        s << Call(2013, month=1, day=15)
-        assert list(s) == [2013, 1, 15, 0]
-
-        s(*args, **kwargs)
-        s(2013, month=1, day=15)
-        assert list(s) == [2013, 1, 15, 0]
-
-        args = s(*args, **kwargs)
-        args = s(2013, month=1, day=15)
+        args = signature(*args, **kwargs)
+        args = signature(2013, month=1, day=15)
         assert list(args) == [2013, 1, 15, 0]
