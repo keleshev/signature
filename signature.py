ace64592d253165424becb803d280a9ffdc4ee32
Vladimir Keleshev <halst@ya.ru>
2013-01-16 17:58:34 +0100

    Make a signature return named tuple of arguments

--- signature.py
+++ signature.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 class SignatureError(Exception):
     pass
 
@@ -26,26 +29,21 @@ class Signature(object):
         self._params = map(Param, params)
         # TODO test, implement that keyword params follow positional params
 
-    def __iter__(self):
-        return (self.__dict__[p.name] for p in self._params)
-
-    def __getitem__(self, key):
-        return list(iter(self))[key]
-
     def __call__(self, *args, **kwargs):
-        self.__dict__ = {'_params': self._params}  # reset __dict__
         if len(args) > len(self._params):
             raise SignatureError()  # TODO better error reporting
         if len(args) < len([p for p in self._params if p.default is Nothing]):
             raise SignatureError()  # TODO test
         # Pad `args` to make it's length same as `self._params`
         args = args + (Nothing,) * (len(self._params) - len(args))
+        values = {}
         for param, arg in zip(self._params, args):
             if arg is Nothing:
                 if param.default is Nothing:
                     assert not 'reachable'
                 else:
-                    self.__dict__[param.name] = param.default
+                    values[param.name] = param.default
             else:
-                self.__dict__[param.name] = arg
-        return self
+                values[param.name] = arg
+        Arguments = namedtuple('Argument', [p.name for p in self._params])
+        return Arguments(**values)
--- test_signature.py
+++ test_signature.py
@@ -7,25 +7,25 @@ def test_positional_parameters_vs_positional_arguments():
 
     signature = Signature('year', 'month', 'day')
 
-    args = signature(*[2013, 1, 15])
+    args = signature(2013, 1, 15)
     assert args.year  == args[0] == 2013
     assert args.month == args[1] == 1
     assert args.day   == args[2] == 15
-    assert list(args) == [2013, 1, 15]
+    assert args == (2013, 1, 15)
 
     with raises(SignatureError):
-        signature(*[])
+        signature()
     with raises(SignatureError):
-        signature(*[2013, 1, 15, 20, 44])
+        signature(2013, 1, 15, 20, 44)
 
 
 def test_keyword_parameters_vs_positional_arguments():
 
     signature = Signature(('year', 1970), ('month', 1), ('day', 1))
 
-    assert list(signature(2013, 1, 15)) == [2013, 1, 15]
-    assert list(signature(2013)) == [2013, 1, 1]
-    assert list(signature()) == [1970, 1, 1]
+    assert signature(2013, 1, 15) == (2013, 1, 15)
+    assert signature(2013) == (2013, 1, 1)
+    assert signature() == (1970, 1, 1)
 
     with raises(SignatureError):
         signature(2013, 1, 15, 20, 44)
@@ -35,8 +35,8 @@ def test_mixed_parameters_vs_positional_arguments():
 
     signature = Signature('year', ('month', 1), ('day', 1))
 
-    assert list(signature(2013, 1, 15)) == [2013, 1, 15]
-    assert list(signature(2013)) == [2013, 1, 1]
+    assert signature(2013, 1, 15) == (2013, 1, 15)
+    assert signature(2013) == (2013, 1, 1)
 
     with raises(SignatureError):
         signature()
@@ -51,4 +51,4 @@ def test_mixed_parameters_vs_mixed_arguments():
     if 0:
         args = signature(*args, **kwargs)
         args = signature(2013, month=1, day=15)
-        assert list(args) == [2013, 1, 15, 0]
+        assert args == (2013, 1, 15, 0)
